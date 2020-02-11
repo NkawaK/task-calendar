6cbeb83a64f9dc79044d51f4b4d5d18e09da805e
西川健太 <kentawest83@gmail.com>
2020-02-11 11:54:14 +0900

    Add modal overlay

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -77,44 +77,45 @@ const Modal: React.FC<Props> = (props) => {
 
   if (props.modalOn) {
     return (
-      <div className="modal">
+      <div>
+        <div className="modal" onClick={() => props.modalSet("", false)}></div>
         <div className="modal_content">
-          <div className="modal_content_close" onClick={() => props.modalSet("", false)}>
-            <span className="modal_content_close_button">×</span>
+            <div className="modal_content_close" onClick={() => props.modalSet("", false)}>
+              <span className="modal_content_close_button">×</span>
+            </div>
+            <div className="modal_form_area">
+              <form className="modal_content_form" onSubmit={e => setTask(e)}>
+                <div className="modal_content_form_title">
+                  <label htmlFor="title">Title</label>
+                  <input type="text" id="title" value={title} onChange={e => setTitle(e.target.value)}/>
+                </div>
+                <div>
+                  <label htmlFor="complete">Complete</label>
+                  <input type="checkbox" id="complete" onClick={() => setComplete(`${!(complete === "true")}`)} checked={complete === "true"}/>
+                </div>
+                <div className="modal_content_form_date">
+                  <label htmlFor="date">Date</label>
+                  <DatePicker
+                    selected={props.startDate}
+                    onChange={handleDateChange}
+                  />
+                </div>
+                <div className="modal_content_form_details">
+                  <label htmlFor="description">Detail</label>
+                  <textarea name="description" id="description" 
+                    value={detail} 
+                    onChange={e => setDetail(e.target.value)}>
+                  </textarea>
+                </div>
+                <div className="modal_button_area">
+                  {taskId? <button className="delete_button" type="button" onClick={deleteTask}>Delete</button> : null}
+                  <button className="modal_content_from_button" type="submit">
+                    {taskId? "Edit" : "Add"}
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-          <div className="modal_form_area">
-            <form className="modal_content_form" onSubmit={e => setTask(e)}>
-              <div className="modal_content_form_title">
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" value={title} onChange={e => setTitle(e.target.value)}/>
-              </div>
-              <div>
-                <label htmlFor="complete">Complete</label>
-                <input type="checkbox" id="complete" onClick={() => setComplete(`${!(complete === "true")}`)} checked={complete === "true"}/>
-              </div>
-              <div className="modal_content_form_date">
-                <label htmlFor="date">Date</label>
-                <DatePicker
-                  selected={props.startDate}
-                  onChange={handleDateChange}
-                />
-              </div>
-              <div className="modal_content_form_details">
-                <label htmlFor="description">Detail</label>
-                <textarea name="description" id="description" 
-                  value={detail} 
-                  onChange={e => setDetail(e.target.value)}>
-                </textarea>
-              </div>
-              <div className="modal_button_area">
-                {taskId? <button className="delete_button" type="button" onClick={deleteTask}>Delete</button> : null}
-                <button className="modal_content_from_button" type="submit">
-                  {taskId? "Edit" : "Add"}
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
       </div>
     );
   } else {
